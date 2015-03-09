e58ecff66f5d0a581329a96a5d68aacb5f13a482
henrybw <henry.babaweiss@gmail.com>
2015-03-09 02:40:37 -0700

    Wipe out temp code in life.nim

--- src/life.nim
+++ src/life.nim
@@ -1,8 +1,5 @@
 import core
 
 when isMainModule:
-    var univ = newUniverse(width = 400, height = 300)
-    echo("Height: ", univ.height)
-    echo("1,1 age: ", univ.cellAt(1, 1).age)
-    univ.evolve
-    echo("1,1 age: ", univ.cellAt(1, 1).age)
+    # TODO: implment driver system
+    echo()
